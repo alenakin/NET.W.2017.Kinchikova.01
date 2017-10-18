04dbb7224fae2b34bee0429225c2d087eef9a38b
alenakin <alenakin1@gmail.com>
2017-10-18 21:39:36 +0300

    QuickSort and MergeSort methods created

---
+++ ConsolUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Logic.SortingClass;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 2, 20, 3, 4, 31, 7, 4, 2, 1, 10 };
+            int[] array2 = { 2, 1, 23, 5, 5, 2, 0, -2 };
+            Console.WriteLine("Source array1: ");
+            ShowArray(array1);
+            Console.WriteLine("Quick sort: ");
+            QuickSort(array1);
+            ShowArray(array1);
+            Console.WriteLine("Source array2: ");
+            ShowArray(array2);
+            Console.WriteLine("Merge sort: ");
+            MergeSort(array2);
+            ShowArray(array2);
+        }
+
+        static void ShowArray(int[] array)
+        {
+            foreach (int i in array)
+                Console.Write(i + " ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Logic/SortingClass.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class SortingClass
+    {
+        public static void QuickSort(int[] array)
+        {
+            QSort(array, 0, array.Length - 1);
+        }
+
+        private static int Partition(int[] array, int left, int right)
+        {
+            int marker = left;
+            for (int i = left; i <= right; i++)
+            {
+                if (array[i] < array[right])
+                {
+                    Swap(ref array[marker], ref array[i]);
+                    marker++;
+                }
+            }
+            Swap(ref array[marker], ref array[right]);
+            return marker;
+        }
+
+        private static void QSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int p = Partition(array, left, right);
+                QSort(array, left, p - 1);
+                QSort(array, p + 1, right);
+            }
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void MergeSort(int[] array)
+        {
+            MSort(array, 0, array.Length);
+        }
+
+        static void MSort(int[] array, int left, int right)
+        {
+            if (left + 1 < right)
+            {
+                int m = (left + right) / 2;
+                MSort(array, left, m);
+                MSort(array, m, right);
+                Merge(array, left, m, right);
+            }
+        }
+
+        static void Merge(int[] array, int left, int m, int right)
+        {
+            int l = 0, r = 0;
+            int size = right - left;
+            int[] result = new int[size];
+            while (left + l < m && m + r < right)
+                if (array[left + l] < array[m + r])
+                {
+                    result[l + r] = array[left + l];
+                    l++;
+                }
+                else
+                {
+                    result[l + r] = array[m + r];
+                    r++;
+                }
+
+            while (left + l < m)
+            {
+                result[l + r] = array[left + l];
+                l++;
+            }
+
+            while (m + r < right)
+            {
+                result[l + r] = array[m + r];
+                r++;
+            }
+
+            for (int i = 0; i < size; i++)
+                array[left + i] = result[i];
+        }
+    }
+}
